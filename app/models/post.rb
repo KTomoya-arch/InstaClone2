34b6335d803a952dd32df6a154b7275786c74620
KTomoya-arch <kato.eng.dao@gmail.com>
2021-09-03 21:37:34 +0900

    CRUD機能

---
+++ app/models/post.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  images     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+class Post < ApplicationRecord
+  belongs_to :user
+  mount_uploaders :images, PostImageUploader
+  serialize :images, JSON
+
+  validates :images, presence: true
+  validates :body, presence: true, length: { maximum: 1000 }
+end
