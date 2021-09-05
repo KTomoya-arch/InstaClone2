e702f726d72f45cf2a3dac5137f71a1bb682e2b8
KTomoya-arch <kato.eng.dao@gmail.com>
2021-09-05 12:04:59 +0900

    コメント機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
   def index
     @posts = Post.all.includes(:user).page(params[:page])
+  end
 
   def new
     @post = Post.new
@@ -33,6 +34,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.order(created_at: :desc)
+    @comment = Comment.new
   end
 
   def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,24 +1,22 @@
 # == Schema Information
 #
-# Table name: posts
+# Table name: comments
 #
 #  id         :integer          not null, primary key
 #  body       :text
-#  images     :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  post_id    :integer
 #  user_id    :integer
 #
 # Indexes
 #
-#  index_posts_on_user_id  (user_id)
+#  index_comments_on_post_id  (post_id)
+#  index_comments_on_user_id  (user_id)
 #
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: :destroy
-  mount_uploaders :images, PostImageUploader
-  serialize :images, JSON
+  belongs_to :post
 
-  validates :images, presence: true
   validates :body, presence: true, length: { maximum: 1000 }
 end
