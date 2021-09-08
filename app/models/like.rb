0062fdd401219e4af3e350a23d346dab49bc75d8
KTomoya-arch <kato.eng.dao@gmail.com>
2021-09-08 21:47:55 +0900

    いいね機能の実装

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,23 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_likes_on_post_id  (post_id)
+#  index_likes_on_user_id  (user_id)
+#
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # likeモデルにおいてuser_idはpost_idが同じであれば異なっていなければならないということ
+  # post_idが異なれば問題はないというのがscopeの意味
+  validates :user_id, uniqueness: { scope: :post_id }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :posts, dependent: :destroy
-  has_many :likes. dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :likes_posts, through: :likes, source: :post
+
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
@@ -30,5 +32,20 @@ class User < ApplicationRecord
 
   def own?(object)
     id == object.user_id
+  end
+  
+  # like_postsに対してpostを破壊的に追加する
+  # https://docs.ruby-lang.org/ja/latest/method/Array/i/=3c=3c.html
+  def like(post)
+    likes_posts << post
+  end
+
+  def unlike(post)
+    likes_posts.destroy(post)
+  end
+
+  # Like.where(user_id: id, post_id: post.id).exist?と同義のよう
+  def like?(post)
+    likes_posts.include?(post)
   end
 end
